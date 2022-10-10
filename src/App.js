e4d824370688e23246263095419ae5871adf4c07
ofego favour <ofegofavour@gmail.com>
2022-10-10 14:04:11 +0100

    meme-one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
 import Header from './Components/Header';
-
+import Meme from './Components/Meme';
 
 function App() {
   return (
      <div>
         <Header />
+        <Meme />
      </div>
   );
 }
--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const Meme = () => {
-  return (
-    <div>Meme</div>
-  )
+import React from "react"
+import '../css/Meme.css';
+export default function Meme(){
+    return(
+        <main>
+            <form className="form">
+                <input
+                 type="text"
+                 placeholder="Top text"
+                 className="form--input"
+                 />
+                <input 
+                   type="text"
+                   placeholder="Bottom text"
+                   className="form--input"
+                   />
+                <button 
+                className="form--button">Get a new image </button>
+            </form>
+        </main>
+    )
 }
-
-export default Meme
