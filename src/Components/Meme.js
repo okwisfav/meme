e2dce1052bdb6fd105e66751cb4ac81fbb0227c8
ofego favour <ofegofavour@gmail.com>
2022-10-30 20:51:25 +0100

    mene

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,10 +1,20 @@
 import React from "react"
 import '../css/Meme.css';
+import memesData from "../data/memesData"
 export default function Meme(){
+
+    let url
+    function getMemeImage () {
+       const memesArray = memesData.data.memes
+       const randomNumber = Math.floor(Math.random() * memesArray.length)
+       url = memesArray[randomNumber].url
+       console.log(url)
+    }
     return(
         <div>
             <form className="form">
-                <input
+                <p>{url}</p>
+                 <input
                  type="text"
                  placeholder="Top text"
                  className="form--input"
@@ -13,6 +23,7 @@ export default function Meme(){
                    type="text"
                    placeholder="Bottom text"
                    className="form--input"
+                   onClick={getMemeImage}
                    />
                 <button 
                 className="form--button">Get a new image </button>
